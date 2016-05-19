864b13c08a7c099e1805a936f5e2f0a6a35a5cc8
Mina S. Younan <mina.younan.cs@gmail.com>
2016-05-19 15:38:30 +0300

    Base-station and gateway
    base-station application is written in C# and gateway in Arduino using
    Arduino-Uno. they can speak the same language (command message)

---
+++ serial communication/serial communication/Send_Recieve.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace serial_communication
+{
+    partial class Send_Recieve
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.stop_button = new System.Windows.Forms.Button();
+            this.start_button = new System.Windows.Forms.Button();
+            this.msg_listbox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.button13);
+            this.groupBox2.Controls.Add(this.button12);
+            this.groupBox2.Controls.Add(this.button10);
+            this.groupBox2.Controls.Add(this.button9);
+            this.groupBox2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.Yellow;
+            this.groupBox2.Location = new System.Drawing.Point(12, 79);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(331, 84);
+            this.groupBox2.TabIndex = 28;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "System Controls";
+            // 
+            // button13
+            // 
+            this.button13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.ForeColor = System.Drawing.Color.Navy;
+            this.button13.Location = new System.Drawing.Point(217, 50);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(108, 28);
+            this.button13.TabIndex = 29;
+            this.button13.Text = "fan_on";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.ForeColor = System.Drawing.Color.Navy;
+            this.button12.Location = new System.Drawing.Point(15, 50);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(110, 28);
+            this.button12.TabIndex = 28;
+            this.button12.Text = "fan_condition_off";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.Color.Navy;
+            this.button10.Location = new System.Drawing.Point(217, 20);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(108, 28);
+            this.button10.TabIndex = 26;
+            this.button10.Text = "fan_off";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.Color.Navy;
+            this.button9.Location = new System.Drawing.Point(15, 20);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(110, 28);
+            this.button9.TabIndex = 25;
+            this.button9.Text = "Fan_on_temp>value";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.stop_button);
+            this.groupBox1.Controls.Add(this.start_button);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Lime;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(276, 61);
+            this.groupBox1.TabIndex = 27;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Serial communication settings";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(15, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(110, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // stop_button
+            // 
+            this.stop_button.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stop_button.ForeColor = System.Drawing.Color.Navy;
+            this.stop_button.Location = new System.Drawing.Point(215, 22);
+            this.stop_button.Name = "stop_button";
+            this.stop_button.Size = new System.Drawing.Size(56, 25);
+            this.stop_button.TabIndex = 4;
+            this.stop_button.Text = "STOP";
+            this.stop_button.UseVisualStyleBackColor = true;
+            this.stop_button.Click += new System.EventHandler(this.stop_button_Click);
+            // 
+            // start_button
+            // 
+            this.start_button.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start_button.ForeColor = System.Drawing.Color.Navy;
+            this.start_button.Location = new System.Drawing.Point(151, 22);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(56, 25);
+            this.start_button.TabIndex = 3;
+            this.start_button.Text = "START";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // msg_listbox
+            // 
+            this.msg_listbox.BackColor = System.Drawing.Color.LightGray;
+            this.msg_listbox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.msg_listbox.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msg_listbox.FormattingEnabled = true;
+            this.msg_listbox.ItemHeight = 17;
+            this.msg_listbox.Location = new System.Drawing.Point(12, 175);
+            this.msg_listbox.Name = "msg_listbox";
+            this.msg_listbox.Size = new System.Drawing.Size(331, 242);
+            this.msg_listbox.TabIndex = 26;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Navy;
+            this.button1.Location = new System.Drawing.Point(294, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 39);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Help";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Send_Recieve
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(355, 429);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.msg_listbox);
+            this.Name = "Send_Recieve";
+            this.Text = "Send_Recieve";
+            this.Load += new System.EventHandler(this.Send_Recieve_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button stop_button;
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.ListBox msg_listbox;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ serial communication/serial communication/Send_Recieve.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using DataSet_Generator;
+
+namespace serial_communication
+{
+    public partial class Send_Recieve : Form
+    {
+        private SerialPort serialPort = new SerialPort();
+        // Threads
+        Thread t;
+        ManualResetEvent runThread = new ManualResetEvent(false);
+
+        // Delegates
+        private delegate void DelegateAddToList(string msg);
+        private DelegateAddToList m_DelegateAddToList;
+        private delegate void DelegateStopPerfmormClick();
+        private DelegateStopPerfmormClick m_DelegateStop;
+
+        public Send_Recieve()
+        {
+            InitializeComponent();
+            string[] allSerialPorts = SerialPort.GetPortNames();
+            comboBox1.DataSource = allSerialPorts;
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void Send_Recieve_Load(object sender, EventArgs e)
+        {
+
+            m_DelegateAddToList = new DelegateAddToList(AddToList);
+            m_DelegateStop = new DelegateStopPerfmormClick(close_serialport);
+            //serialPort.DataReceived += new SerialDataReceivedEventHandler(serialPort_DataReceived);
+
+            t = new Thread(ReceiveThread);
+            t.Start();
+        }
+        private void ReceiveThread()
+        {
+            while (true)
+            {
+                runThread.WaitOne(Timeout.Infinite);
+                while (true)
+                {
+                    try
+                    {
+                        // receive data 
+                        string msg = serialPort.ReadLine();
+                       // m_DelegateAddToList.DynamicInvoke(new Object[] { "R: " + msg });
+                       // m_DelegateAddToList.BeginInvoke(msg, null, this);
+                        this.Invoke(this.m_DelegateAddToList, new Object[] { "R: " + msg });
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            this.Invoke(this.m_DelegateStop, new Object[] { });
+                        }
+                        catch { }
+                        runThread.Reset();
+                        break;
+                    }
+                }
+            }
+        }
+        /*
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // blocks until TERM_CHAR is received
+            try
+            {
+                string msg = serialPort.ReadLine();
+                this.Invoke(this.m_DelegateAddToList, new Object[] { "R: " + msg });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                stop_button.PerformClick();
+            }
+        }
+        */
+        private void AddToList(string msg)
+        {
+            int n = msg_listbox.Items.Add(msg);
+            msg_listbox.SelectedIndex = n;
+            msg_listbox.ClearSelected();
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            serialPort.PortName = "COM13";
+            serialPort.BaudRate = 9600;
+            serialPort.NewLine = "\n";
+
+            try
+            {
+                serialPort.Open();
+                serialPort.WriteLine("set_on,3,240");
+                groupBox2.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            int n = msg_listbox.Items.Add("Connection established...");
+            msg_listbox.SelectedIndex = n;
+            msg_listbox.ClearSelected();
+
+            runThread.Set();
+            
+        }
+        private void close_serialport()
+        {
+            try
+            {
+                if (serialPort.IsOpen == true)
+                    serialPort.Close();
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+
+            runThread.Reset();
+            int n = msg_listbox.Items.Add("Connection closed.");
+            msg_listbox.SelectedIndex = n;
+            msg_listbox.ClearSelected();
+        }
+        private void stop_button_Click(object sender, EventArgs e)
+        {
+            close_serialport();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            serialPort.WriteLine("fan_on_temph,6,20");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            serialPort.WriteLine("fan_off_temph,6,20");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            serialPort.WriteLine("set_off,6,20");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            serialPort.WriteLine("set_on,6,20");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            about frm = new about();
+            frm.ShowDialog();
+        }
+
+    }
+}
